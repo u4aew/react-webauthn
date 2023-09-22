debca2ee0324f7621e41b248ae9aa59162eac94b
sergejucaev <dialoggg1994@gmail.com>
2023-09-22 22:10:07 +0300

    Update demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,9 +1,12 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
 import {useWebAuthn} from 'react-hook-webauthn'
 import './App.css';
+// @ts-ignore
+import { QRCode, QRSvg } from 'sexy-qr';
+import Links from "./components/Links";
 
 const rpOptions = {
-  rpId: 'localhost',
+  rpId: 'webauthn.fancy-app.site',
   rpName: 'my super app'
 }
 
@@ -30,9 +33,29 @@ function App() {
     console.log(assertion)
   }, [getAssertion])
 
+  const qrCode = useMemo(() => {
+    const qr =  new QRCode({
+      content: window.location.href,
+      ecl: 'M'
+    });
+    return new QRSvg(qr, {
+      fill: '#182026',
+      cornerBlocksAsCircles: true,
+      size: 200,
+      radiusFactor: 0.75,
+      cornerBlockRadiusFactor: 2,
+      roundOuterCorners: true,
+      roundInnerCorners: true,
+    }).svg
+  }, [])
+
   return (
     <div className="App">
       <main>
+        <div className="title">
+          Webauthn demo
+        </div>
+        <div className={'qrCode'} dangerouslySetInnerHTML={{ __html: qrCode }} />
         <div className="section">
           <input onInput={onChangeLogin} placeholder="login" type="text"/>
         </div>
@@ -43,6 +66,7 @@ function App() {
         <div className="section">
           <button onClick={onAuth} type="button">auth</button>
         </div>
+        <Links />
       </main>
     </div>
   );
