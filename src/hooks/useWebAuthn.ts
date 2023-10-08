ef1bf59b3b6ebdcb147683b5b47867c7032ab94a
sergejucaev <dialoggg1994@gmail.com>
2023-10-08 15:44:10 +0300

    Add JsDoc

--- src/hooks/useWebAuthn.ts
+++ src/hooks/useWebAuthn.ts
@@ -1,3 +1,12 @@
+/**
+ * Interface for WebAuth configurations.
+ *
+ * @interface IWebAuth
+ * @property {string} rpName - The name of the Relying Party.
+ * @property {string} rpId - The Relying Party identifier.
+ * @property {PublicKeyCredentialCreationOptions} [credentialOpt] - Optional options for credential creation.
+ * @property {PublicKeyCredentialRequestOptions} [assertionOpt] - Optional options for assertion.
+ */
 export interface IWebAuth {
   rpName: string;
   rpId: string;
@@ -5,22 +14,50 @@ export interface IWebAuth {
   assertionOpt?: PublicKeyCredentialRequestOptions;
 }
 
+/**
+ * Interface for Assertion Options.
+ *
+ * @interface IAssertionOpt
+ * @property {string} challenge - The challenge for the assertion.
+ */
 export interface IAssertionOpt {
   challenge: string;
 }
 
+/**
+ * Interface for Credential Options. Extends IAssertionOpt.
+ *
+ * @interface ICredentialOpt
+ * @property {string} userId - The user ID.
+ * @property {string} userName - The username.
+ * @property {string} userDisplayName - The display name of the user.
+ */
 export interface ICredentialOpt extends IAssertionOpt {
   userId: string;
   userName: string;
   userDisplayName: string;
 }
-
+/**
+ * Custom hook for WebAuthn.
+ *
+ * @function
+ * @param {IWebAuth} params - The parameters for the WebAuthn.
+ * @returns {object} - The getCredentials and getAssertion methods.
+ */
 export function useWebAuthn({
   rpName,
   rpId,
   credentialOpt,
   assertionOpt,
 }: IWebAuth) {
+  /**
+   * Method to get Credentials.
+   *
+   * @async
+   * @function
+   * @param {ICredentialOpt} params - The parameters for getting credentials.
+   * @returns {Promise<any>} - A Promise that resolves to the credentials.
+   */
   const getCredential = async ({
     userId,
     userName,
@@ -65,7 +102,14 @@ export function useWebAuthn({
       publicKey: publicKeyCredentialCreationOptions,
     });
   };
-
+  /**
+   * Method to get Assertion.
+   *
+   * @async
+   * @function
+   * @param {IAssertionOpt} params - The parameters for getting assertion.
+   * @returns {Promise<any>} - A Promise that resolves to the assertion.
+   */
   const getAssertion = async ({ challenge }: IAssertionOpt) => {
     const publicKeyCredentialRequestOptions = {
       challenge: Uint8Array.from(challenge, (c) => c.charCodeAt(0)),
